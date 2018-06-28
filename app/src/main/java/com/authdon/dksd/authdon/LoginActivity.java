1e45d95bb0358487085831379272751f03541fc8
dsdawkins <dsdawkins@squaretrade.com>
2018-06-28 15:49:04 -0700

    Yeah yeah

--- app/src/main/java/com/authdon/dksd/authdon/LoginActivity.java
+++ app/src/main/java/com/authdon/dksd/authdon/LoginActivity.java
@@ -310,19 +310,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
             try {
                 // Simulate network access.
-                Thread.sleep(2000);
+                //Thread.sleep(2000);
                 //Here we actually send a register message to the backend saving the creds..
                 pushService.send("{register user cerendtials here}");
-            } catch (InterruptedException e) {
+            } catch (Exception e) {
                 return false;
-            }
-
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the phone matches.
-                    return pieces[1].equals(mphone);
-                }
             }
 
             // TODO: register the new account here.
